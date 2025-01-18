c1d686c23fdffe91ad5623ef0c4461d53cdf0f99
Runarok <RunarokHrafn@gmail.com>
2025-01-18 07:57:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse a linked list/reverse-a-linked-list.js
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+// Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => { inputString += inputStdin; });
+
+process.stdin.on('end', _ => {
+    inputString =
+        inputString.trim().split('\n').map(string => { return string.trim(); });
+
+    main();
+});
+
+function readLine() { return inputString[currentLine++]; }
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+function printlist(head) {
+    let current = head;
+    let s = '';
+    while (current !== null) {
+        s += current.data + " ";
+        current = current.next;
+    }
+    console.log(s);
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for (; i < t; i++) {
+
+        let input_ar1 = readLine().split(' ').map(x => parseInt(x));
+        let n = input_ar1.length;
+        let head = new Node(input_ar1[0]);
+        let tail = head;
+        for (let i = 1; i < n; i++) {
+            tail.next = new Node(input_ar1[i]);
+            tail = tail.next;
+        }
+        let obj = new Solution();
+        head = obj.reverseList(head);
+        printlist(head);
+        console.log("~");
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for javascript
+
+/**
+ * @param {Node} head
+ * @returns {Node}
+ */
+
+/*
+class Node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+
+class Solution {
+    reverseList(head) {
+        let prev = null;       // Initially, there is no previous node
+        let current = head;    // Start with the head of the list
+        
+        while (current !== null) {
+            let next = current.next;  // Save the next node
+            current.next = prev;      // Reverse the current node's pointer
+            prev = current;           // Move prev to the current node
+            current = next;           // Move to the next node in the list
+        }
+        
+        return prev;  // At the end, prev will be the new head of the reversed list
+    }
+}
