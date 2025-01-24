80a43d788a298890de0b453be5db83d44b8feaa4
Runarok <RunarokHrafn@gmail.com>
2025-01-24 14:26:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Evaluate Formulae/evaluate-formulae.py
@@ -0,0 +1,24 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+########### Write your code below ###############
+# Complete the code below that stores the output of the expression
+res = (a + b) // c + d
+########### Write your code above ###############
+
+# This prints the output. Don't change it!
+print(res)
+
+
+#{ 
+ # Driver Code Starts.
+
+# } Driver Code Ends
