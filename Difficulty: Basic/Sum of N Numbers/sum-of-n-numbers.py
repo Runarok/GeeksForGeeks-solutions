ef1958944e0f8a55b963eec5ca196f8efd67688b
Runarok <RunarokHrafn@gmail.com>
2025-01-24 14:25:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of N Numbers/sum-of-n-numbers.py
@@ -0,0 +1,21 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+n = int(input())
+
+########### Write your code below ###############
+# Compute the sum of first n natural numbers using the formula
+ans = n * (n + 1) // 2
+########### Write your code above ###############
+
+print(ans)
+
+
+
+#{ 
+ # Driver Code Starts.
+
+# } Driver Code Ends
