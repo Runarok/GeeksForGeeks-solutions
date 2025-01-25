785c2b526224194c36028c91435d367d87c7c938
Runarok <RunarokHrafn@gmail.com>
2025-01-25 14:56:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/AP Term/ap-term.py
@@ -0,0 +1,17 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+a=int(input())
+d=int(input())
+n=int(input())
+# Compute the nth term using the formula
+ans = a + (n - 1) * d
+print(ans)
+
+#{ 
+ # Driver Code Starts.
+
+# } Driver Code Ends
