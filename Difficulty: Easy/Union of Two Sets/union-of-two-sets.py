fd752ae7cfbb66c0874dd764e1d86c002e29aa4e
Runarok <RunarokHrafn@gmail.com>
2025-01-25 14:49:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Union of Two Sets/union-of-two-sets.py
@@ -0,0 +1,16 @@
+
+a = set([int(x) for x in input().strip().split()])
+b = set([int(x) for x in input().strip().split()])
+
+st = a | b
+
+# Printing the size of the set which is the total number of elements in the set.
+print(len(st))
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
