fe5b1741bed26f8b980b806d7d10a17ee3279b0a
Runarok <RunarokHrafn@gmail.com>
2025-01-25 14:37:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Function With No Arguments/function-with-no-arguments.py
@@ -0,0 +1,26 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+def helloFunction():
+    print("Hello")
+
+
+#{ 
+ # Driver Code Starts.
+
+
+
+def main():
+    t=int(input())
+    while(t>0):
+        
+        helloFunction()
+        t-=1
+
+        print("~")
+if __name__ == "__main__": 
+    main()
+# } Driver Code Ends
