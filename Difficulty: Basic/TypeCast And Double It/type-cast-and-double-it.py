e38b30d9a2263f25aa04d727f4c85464f41e8fa4
Runarok <RunarokHrafn@gmail.com>
2025-01-25 14:45:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/TypeCast And Double It/type-cast-and-double-it.py
@@ -0,0 +1,21 @@
+#{ 
+ # Driver Code Starts
+
+# } Driver Code Ends
+
+# Take input as a string
+num = input()
+
+# Typecast the string to an integer
+number = int(num)
+
+# Double the number
+result = number * 2
+
+# Output the result
+print(result)
+
+
+#{ 
+ # Driver Code Starts.
+# } Driver Code Ends
