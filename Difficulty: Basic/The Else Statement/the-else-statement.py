8eca4c308f9a982c8e9f28be2ae076f788327b22
Runarok <RunarokHrafn@gmail.com>
2025-01-25 14:40:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The Else Statement/the-else-statement.py
@@ -0,0 +1,21 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+def check_number(a):
+    if a > 100:
+        print("Big")
+    else:
+        print("Number")
+
+# Take input from the user
+a = int(input())  # Convert input string to integer
+check_number(a)
+
+
+#{ 
+ # Driver Code Starts.
+
+# } Driver Code Ends
