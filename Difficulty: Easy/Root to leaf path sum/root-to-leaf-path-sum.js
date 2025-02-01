498980b28f1956f3e81c010df074dba67e2538c9
Runarok <RunarokHrafn@gmail.com>
2025-02-02 05:10:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Root to leaf path sum/root-to-leaf-path-sum.js
@@ -0,0 +1,148 @@
+//{ Driver Code Starts
+// Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => { inputString += inputStdin; });
+
+process.stdin.on('end', _ => {
+    inputString =
+        inputString.trim().split('\n').map(string => { return string.trim(); });
+
+    main();
+});
+
+function readLine() { return inputString[currentLine++]; }
+
+class Node {
+    constructor(x) {
+        this.key = x;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function buildTree(str) {
+    // Corner Case
+    if (str.length === 0 || str[0] == 'N') return null;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    let ip = new Array();
+
+    let ip_str = str.split(" ");
+
+    for (let i = 0; i < ip_str.length; i++) ip.push(ip_str[i]);
+
+    // Create the root of the tree
+    let root = new Node(parseInt(ip[0]));
+
+    // Push the root to the queue
+    let queue = new Array();
+    queue.push(root);
+
+    // Starting from the second element
+    let i = 1;
+    while (queue.length !== 0 && i < ip.length) {
+
+        // Get and remove the front of the queue
+        let currNode = queue[0];
+        queue.shift();
+
+        // Get the current node's value from the string
+        let currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode.left = new Node(parseInt(currVal));
+
+            // Push it to the queue
+            queue.push(currNode.left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.length) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode.right = new Node(parseInt(currVal));
+
+            // Push it to the queue
+            queue.push(currNode.right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+function main() {
+    let t = parseInt(readLine());
+    for (let i = 0; i < t; i++) {
+        let s_tree = readLine().trim();
+        let S = parseInt(readLine());
+        let root = buildTree(s_tree);
+
+        let obj = new Solution();
+        let res = obj.hasPathSum(root, S);
+        if (res)
+            console.log("true");
+        else
+            console.log("false");
+        console.log("~");
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for javascript
+
+/*
+class Node
+{
+    constructor(x){
+        this.key=x;
+        this.left=null;
+        this.right=null;
+    }
+}
+*/
+
+/**
+ * @param {Node} root
+ * @param {number} S
+ * @return {boolean}
+ */
+class Solution {
+    // Function to check if there is a path from root to leaf with the sum equal to target
+    hasPathSum(root, target) {
+        // Base case: if root is null, return false
+        if (!root) {
+            return false;
+        }
+        
+        // Subtract the current node's value from the target
+        target -= root.key;
+        
+        // If the current node is a leaf, check if target is 0
+        if (!root.left && !root.right) {
+            return target === 0;
+        }
+        
+        // Recursively check for left and right subtrees
+        return this.hasPathSum(root.left, target) || this.hasPathSum(root.right, target);
+    }
+}
