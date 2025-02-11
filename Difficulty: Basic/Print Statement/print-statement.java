fa8da7d3d0be31b1dc1fe1da613ec9fdfa7fc9fa
Runarok <RunarokHrafn@gmail.com>
2025-02-12 04:37:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Statement/print-statement.java
@@ -0,0 +1,23 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+import java.util.*;
+
+public class GFG {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input from the console
+        Scanner scanner = new Scanner(System.in);
+
+        // Read a line of input as a string
+        String inputString = scanner.nextLine();
+
+        // Print the input string and move the cursor to a new line
+        System.out.println(inputString);
+
+        // Close the scanner to prevent resource leaks
+        scanner.close();
+    }
+}
+
+//{ Driver Code Starts.
+// } Driver Code Ends
