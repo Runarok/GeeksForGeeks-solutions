9bbc3a3da6d5f73974883d847b9a5d592c4db097
Runarok <RunarokHrafn@gmail.com>
2025-02-14 15:54:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print GeeksForGeeks/print-geeks-for-geeks.cpp
@@ -0,0 +1,17 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Printing "GeeksForGeeks" to the console
+    cout << "GeeksForGeeks\n";
+    return 0;
+}
+
+
+//{ Driver Code Starts.
+
+// } Driver Code Ends
