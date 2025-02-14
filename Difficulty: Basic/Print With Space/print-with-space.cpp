e3991bb4ce92a1f76d2a6fc3c35e8d44c740b302
Runarok <RunarokHrafn@gmail.com>
2025-02-14 15:44:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print With Space/print-with-space.cpp
@@ -0,0 +1,23 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a;
+    getline(cin, a);  // Read the first string
+    string b;
+    getline(cin, b);  // Read the second string
+
+    // Print both strings separated by a space and then a newline
+    cout << a << " " << b << endl;
+
+    return 0;
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
