932572e8aa7cfff90c2adf78109c5e6c638597ee
Runarok <RunarokHrafn@gmail.com>
2025-02-15 12:34:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The Elif Statement/the-elif-statement.py
@@ -0,0 +1,23 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+# } Driver Code Ends
+#User function Template for python3
+
+# Read an integer input
+a = int(input())
+
+# Check the value of 'a' and print the corresponding message
+if a > 100:
+    print("Big")  # If 'a' is greater than 100, print "Big"
+elif a < 10:
+    print("Small")  # If 'a' is less than 10, print "Small"
+else:
+    print("Number")  # If 'a' is between 10 and 100 (inclusive), print "Number"
+
+
+#{ 
+ # Driver Code Starts.
+# } Driver Code Ends
