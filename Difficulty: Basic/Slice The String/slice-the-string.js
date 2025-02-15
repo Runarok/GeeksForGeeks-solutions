fbc4805e60c5d14f7f470e6ee630bbd7cbdfd6a1
Runarok <RunarokHrafn@gmail.com>
2025-02-15 12:51:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Slice The String/slice-the-string.js
+++ Difficulty: Basic/Slice The String/slice-the-string.js
@@ -1,50 +1,42 @@
 //{ Driver Code Starts
-//Initial Template for javascript
-
-'use strict';
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
+// Initial Template for javascript
 
 function main() {
-    let t = parseInt(readLine());
-    let i = 0;
-    for(;i<t;i++)
-    {
-        let str = readLine();
-        let obj = new Solution();
-        console.log(obj.sliceString(str));
-    
-console.log("~");
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input : process.stdin,
+        output : process.stdout,
+    });
+
+    let count = 0;
+    rl.on("line", function(line) {
+        if (count === 0) {
+            count = Number(line.trim());
+        } else if (count > 0) {
+            const s = line.trim();
+            console.log(sliceString(s));
+            console.log("~");
+            count--;
+        }
+        if (count === 0) rl.close();
+    });
 }
-}
+
+main();
 
 // } Driver Code Ends
 
+// User function Template for JavaScript
 
-//User function Template for javascript
-class Solution {
-    sliceString(str) {
-        // Slice the string starting from index 1 to the second-to-last index
-        return str.slice(1, str.length - 1);
-    }
+/**
+ * @param {string} s
+ * @returns {string}
+ */
+
+function sliceString(s) {
+    // Extract the substring by removing the first and last character
+    let slicedText = s.slice(1, s.length - 1);
+    
+    // Return the modified string
+    return slicedText;
 }
