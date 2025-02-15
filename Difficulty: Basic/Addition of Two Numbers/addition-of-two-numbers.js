bb67d95ae447b126e88de78c24ac716db87b0902
Runarok <RunarokHrafn@gmail.com>
2025-02-15 12:32:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Addition of Two Numbers/addition-of-two-numbers.js
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => { inputString += inputStdin; });
+
+process.stdin.on('end', _ => {
+    inputString =
+        inputString.trim().split('\n').map(string => { return string.trim(); });
+
+    main();
+});
+
+function readLine() { return inputString[currentLine++]; }
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for (; i < t; i++) {
+        let [A, B] = readLine().trim().split(" ").map((x) => parseInt(x));
+        let obj = new Solution();
+        let res = obj.addition(A, B);
+        console.log(res);
+
+        console.log("~");
+    }
+}
+
+// } Driver Code Ends
+
+// User function Template for JavaScript
+
+/**
+ * @param {number} A - First number
+ * @param {number} B - Second number
+ * @return {number} - Sum of A and B
+ */
+
+class Solution {
+    addition(a, b) {
+        // Return the sum of two numbers
+        return a + b;
+    }
+}
