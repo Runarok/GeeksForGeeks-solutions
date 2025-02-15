643995031b81255f8f80ddf8273a5c0d8781f0a3
Runarok <RunarokHrafn@gmail.com>
2025-02-15 12:43:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Multi Printing/multi-printing.py
@@ -0,0 +1,19 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+# User function Template for python3
+
+a = input()  # Read the input string
+n = int(input())  # Read the integer input
+
+# Print the string 'a' repeated 'n' times without a newline
+for i in range(n):
+    print(a, end="")
+
+
+#{ 
+ # Driver Code Starts.
+# } Driver Code Ends
