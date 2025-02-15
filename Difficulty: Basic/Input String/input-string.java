74906e5b46e9d5013019fed13da5c51c8ee246fc
Runarok <RunarokHrafn@gmail.com>
2025-02-15 12:55:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Input String/input-string.java
@@ -0,0 +1,26 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        // Create a Scanner object to take input from the user
+        Scanner sc = new Scanner(System.in);
+        
+        // Read the entire line as a string input
+        String inputString = sc.nextLine();
+        
+        // Print the input string
+        System.out.println(inputString);
+        
+        // Close the scanner to prevent resource leak
+        sc.close();
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
