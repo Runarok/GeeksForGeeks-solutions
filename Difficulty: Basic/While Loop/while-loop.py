6fe84288ee2d025a1ef2c90d09572781fe5cd748
Runarok <RunarokHrafn@gmail.com>
2025-02-15 12:31:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/While Loop/while-loop.py
@@ -0,0 +1,21 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+# User function Template for Python3
+
+# Read an integer input
+x = int(input())
+
+# Print numbers from x down to 0 in a single line
+for i in range(x, -1, -1):
+    print(i, end=" ")
+
+
+
+#{ 
+ # Driver Code Starts.
+
+# } Driver Code Ends
