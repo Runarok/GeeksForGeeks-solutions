a671619ec65e9452de647647599fd589b16f569c
Runarok <RunarokHrafn@gmail.com>
2025-02-15 12:32:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Logical Operators/logical-operators.py
@@ -0,0 +1,32 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+# User function Template for Python3
+
+# The two lines below take input. Don't change them!
+a = int(input())
+b = int(input())
+
+########### Write your code below ###############
+
+# Perform logical AND operation between a and b
+p = a and b
+
+# Perform logical OR operation between a and b
+q = a or b
+
+# Perform logical NOT operation on a
+r = not a
+
+########### Write your code above ###############
+
+# The code below prints the output. Don't change it!
+print(p, q, r)
+
+
+#{ 
+ # Driver Code Starts.
+# } Driver Code Ends
