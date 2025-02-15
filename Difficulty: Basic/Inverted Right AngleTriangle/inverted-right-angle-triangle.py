04b3738b0e6f4a97940af5027fe0c3ae57e6adcf
Runarok <RunarokHrafn@gmail.com>
2025-02-15 12:35:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Inverted Right AngleTriangle/inverted-right-angle-triangle.py
@@ -0,0 +1,23 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+# User function Template for python3
+
+# Read an integer input from the user
+n = int(input())
+
+# Loop to print the pattern
+for row in range(n):  
+    for col in range(n - row):  
+        print("*", end=" ")  # Print a star followed by a space
+    print()  # Move to the next line after printing one row
+
+
+
+#{ 
+ # Driver Code Starts.
+
+# } Driver Code Ends
