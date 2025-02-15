14b75ac488d810b779b31135a133b2c407a85273
Runarok <RunarokHrafn@gmail.com>
2025-02-15 12:37:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/For Loop - 1/for-loop-1.py
@@ -0,0 +1,21 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+
+# User function Template for Python3
+
+# Take input for the number
+n = int(input())
+
+# Loop to print the multiplication table from 1 to 10
+for i in range(1, 11):
+    print(n * i, end=" ")
+
+
+#{ 
+ # Driver Code Starts.
+
+# } Driver Code Ends
