27c3670da826220725fae051b846c130c98e0f9f
Runarok <RunarokHrafn@gmail.com>
2025-02-15 12:28:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Implement Union/implement-union.c
@@ -0,0 +1,27 @@
+//{ Driver Code Starts
+// Initial Template for C
+
+// } Driver Code Ends
+
+// User function Template for C
+
+/* Define the binary tree node structure */
+struct NODE {
+    double data;           // Node value
+    struct NODE* left;     // Pointer to left child
+    struct NODE* right;    // Pointer to right child
+};
+
+
+//{ Driver Code Starts.
+#include <stdio.h>
+
+int main() {
+    // code
+
+    struct NODE ll;
+    printf("%d\n", 1);
+    printf("~");
+    return 0;
+}
+// } Driver Code Ends
