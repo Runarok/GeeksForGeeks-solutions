ca0c9dc56eb6bf8eb61851d97b6ba8e1ab6cd99a
Runarok <RunarokHrafn@gmail.com>
2025-02-17 02:57:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Lambda Expression in C++/lambda-expression-in-c.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+
+int main() 
+{ 
+    int t; cin>>t;
+    
+    while(t--){
+        int x;
+        cin>>x;
+        
+        
+// } Driver Code Ends
+//User function Template for C++
+
+// Define a lambda function to calculate the square of a number
+auto square = [](int y) { return y * y; };
+
+
+//{ Driver Code Starts.
+        
+        cout<<square(x)<<endl;
+    
+cout << "~" << "\n";
+}
+  
+} 
+// } Driver Code Ends
