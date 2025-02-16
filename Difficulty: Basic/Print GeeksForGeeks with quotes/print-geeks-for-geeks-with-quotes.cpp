05ba9afd70df5c607c80ad7fc7f84746ef681c4b
Runarok <RunarokHrafn@gmail.com>
2025-02-16 06:40:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print GeeksForGeeks with quotes/print-geeks-for-geeks-with-quotes.cpp
@@ -0,0 +1,16 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Printing the string "GeeksForGeeks" with escaped double quotes
+    cout << "\"GeeksForGeeks\"";
+    return 0;
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
