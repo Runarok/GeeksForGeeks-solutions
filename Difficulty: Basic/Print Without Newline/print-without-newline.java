e42fd1d5c569b86dc69a6fdff5b16a612f37fbb9
Runarok <RunarokHrafn@gmail.com>
2025-02-16 06:59:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Without Newline/print-without-newline.java
@@ -0,0 +1,23 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // Create Scanner object to read input
+        Scanner sc = new Scanner(System.in);
+
+        // Read two input strings
+        String firstInput = sc.nextLine();
+        String secondInput = sc.nextLine();
+
+        // Print both strings in the required format without a new line
+        System.out.print(firstInput + " " + secondInput);
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
