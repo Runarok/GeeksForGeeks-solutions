8dc91a658c857276168619688392807389881ff2
Runarok <RunarokHrafn@gmail.com>
2025-02-16 07:21:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Compare two numbers/compare-two-numbers.cpp
@@ -0,0 +1,27 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare two integer variables to store input values
+    int a, b;
+    
+    // Read input values from standard input
+    cin >> a >> b;
+    
+    // Compare values and print the appropriate result
+    if (a <= b) {
+        cout << "0"; // Print 0 if 'a' is less than or equal to 'b'
+    } else {
+        cout << "1"; // Print 1 if 'a' is greater than 'b'
+    }
+    
+    return 0;
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
