311e37a44b060fed1580bf5ea3529946fba071dd
Runarok <RunarokHrafn@gmail.com>
2025-02-17 02:58:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Convert to Hexadecimal/convert-to-hexadecimal.cpp
@@ -0,0 +1,18 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;  // Input the integer
+    
+    // Print the hexadecimal value of 'a' and move to the next line
+    cout << hex << a << endl;
+
+    return 0;
+}
+
+//{ Driver Code Starts.
+// } Driver Code Ends
