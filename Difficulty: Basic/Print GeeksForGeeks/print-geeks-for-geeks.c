ea98f7b922761fdb88702d84fd2ebbefc692cccb
Runarok <RunarokHrafn@gmail.com>
2025-02-17 18:52:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print GeeksForGeeks/print-geeks-for-geeks.c
@@ -0,0 +1,21 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+#include <stdio.h>
+
+int main() {
+
+    // comment this line
+    // printf("Hello World\n");
+
+    // prints GeeksforGeeks
+    printf("GeeksForGeeks\n");
+
+    return 0;
+}
+
+
+//{ Driver Code Starts.
+
+// } Driver Code Ends
