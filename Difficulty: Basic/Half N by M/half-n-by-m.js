cb3102c18da0fd3ac5a1ee61c93a67b723044a77
Runarok <RunarokHrafn@gmail.com>
2025-02-17 18:03:09 +0530

    Update half-n-by-m.js

--- Difficulty: Basic/Half N by M/half-n-by-m.js
+++ Difficulty: Basic/Half N by M/half-n-by-m.js
@@ -58,24 +58,25 @@ console.log("~");
 // User function Template for javascript
 
 /**
- * @param {number} n
- * @param {number} m
- * @returns {number}
+ * @param {number} n - The number to be halved repeatedly
+ * @param {number} m - The number of times to halve the number
+ * @returns {number} - The value after halving the number (m-1) times
  */
 
 class Solution{
     mthHalf(n, m){
-        // code here
+        // If m is greater than 1, we proceed to halve the number
         if(m > 1){
-            let ans = []
-            let val = n
+            let results = []   // To store the intermediate results after each halving
+            let currentValue = n  // Initialize with the original number
             for(let i = 0; i < m; i++){
-                val = val / 2
-                ans.push(Math.floor(val))
+                currentValue = currentValue / 2   // Halve the current value
+                results.push(Math.floor(currentValue))  // Store the floored value after halving
             }
-            return ans[ans.length - 2]
+            return results[results.length - 2]  // Return the value before the last halving step
         } else {
-            return n
+            return n  // If m is not greater than 1, simply return the original number
         }
     }
 }
+
