928504b8fe67a9d9ceef47e176315be2b7b0926d
Runarok <RunarokHrafn@gmail.com>
2025-02-17 15:04:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Int Str/int-str.py
@@ -0,0 +1,20 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+# User function Template for Python3
+
+# Taking inputs
+a = input()
+b = input()
+c = input()
+
+# Printing 'a' repeated 'a' times, followed by 'b' repeated 'b' times, separated by 'c'
+print(a * int(a), b * int(b), sep=c)
+
+
+#{ 
+ # Driver Code Starts.
+# } Driver Code Ends
