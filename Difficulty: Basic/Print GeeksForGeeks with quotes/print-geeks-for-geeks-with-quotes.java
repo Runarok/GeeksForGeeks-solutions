a891af3bebed4f3671bbb32279c7af596b6b0313
Runarok <RunarokHrafn@gmail.com>
2025-02-17 18:13:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print GeeksForGeeks with quotes/print-geeks-for-geeks-with-quotes.java
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        Solution ob = new Solution();
+        while (t-- > 0) {
+            ob.print();
+            System.out.println();
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+// User function Template for Java
+
+class Solution {
+    void print() {
+        // Print the string with double quotes around it
+        System.out.println("\"GeeksForGeeks\"");
+    }
+}
