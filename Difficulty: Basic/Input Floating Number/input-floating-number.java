0a370bf310419d457a12fd7a0f8af4741c1d5d2a
Runarok <RunarokHrafn@gmail.com>
2025-02-17 18:17:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Input Floating Number/input-floating-number.java
@@ -0,0 +1,23 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        // Take a Float input from the user
+        Scanner sc = new Scanner(System.in);
+        
+        // Read the float value
+        float n = sc.nextFloat();
+        
+        // Print the float value multiplied by 10
+        System.out.print(n * 10);
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
