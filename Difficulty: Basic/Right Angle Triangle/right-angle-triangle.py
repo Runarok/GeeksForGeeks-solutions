8485c97a971e2095b3e7c7262d7fc9f028c266ad
Runarok <RunarokHrafn@gmail.com>
2025-02-17 14:59:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Right Angle Triangle/right-angle-triangle.py
@@ -0,0 +1,22 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+# User function Template for Python3
+
+# Input the number of rows
+n = int(input())
+
+# Print the pattern of stars
+for i in range(n):
+    for j in range(i + 1):
+        # Print a star followed by a space
+        print("*", end=" ")
+    # Move to the next line after each row
+    print()
+
+#{ 
+ # Driver Code Starts.
+# } Driver Code Ends
