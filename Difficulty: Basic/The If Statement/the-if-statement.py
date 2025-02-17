559faae53b3a0ac057a7a0d19ddd82ee6f23c47e
Runarok <RunarokHrafn@gmail.com>
2025-02-17 18:58:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The If Statement/the-if-statement.py
@@ -0,0 +1,21 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+# Read input
+n = int(input())
+
+# Check if n is greater than 100
+if n > 100:
+    print("Big")
+
+# Always print "Number"
+print("Number")
+
+
+#{ 
+ # Driver Code Starts.
+
+# } Driver Code Ends
