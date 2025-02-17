4ab258a23b7fdbb30d4edb7717e078aaf6b4e913
Runarok <RunarokHrafn@gmail.com>
2025-02-17 14:26:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Delete alternate characters/delete-alternate-characters.js
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function printList(res,n){
+    let s="";
+    for(let i=0;i<n;i++){
+        s+=res[i];
+        s+=" ";
+    }
+    console.log(s);
+}
+
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let S = readLine();
+        let obj = new Solution();
+        let res = obj.delAlternate(S);
+        console.log(res);
+        
+    
+console.log("~");
+}
+}// } Driver Code Ends
+
+
+
+// } Driver Code Ends
+
+// User function Template for JavaScript
+
+/**
+ * @param {String} inputString - The input string from which alternate characters will be removed.
+ * @returns {String} - The modified string containing only characters at even indices.
+ */
+class Solution {
+    delAlternate(inputString) {
+        // Convert the string into an array, filter characters at even indices, and join back into a string
+        return [...inputString].filter((_, index) => index % 2 === 0).join("");
+    }
+}
