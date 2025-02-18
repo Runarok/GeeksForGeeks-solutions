362088c9254b818ee50840886bcaff2d60d05181
Runarok <RunarokHrafn@gmail.com>
2025-02-18 17:50:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Learn to Comment - Python/learn-to-comment-python.py
@@ -0,0 +1,25 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+# Taking input
+a = int(input())  # Input for variable 'a'
+b = int(input())  # Input for variable 'b'
+c = int(input())  # Input for variable 'c'
+
+# Prints a
+print(a)
+
+# Prints b
+#print(b)  # Commented out the print statement for 'b'
+
+# Prints c
+print(c)
+
+
+#{ 
+ # Driver Code Starts.
+
+# } Driver Code Ends
