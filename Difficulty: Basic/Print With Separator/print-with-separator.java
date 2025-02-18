1aed9312f0e11e5cb9ced70716dcdb22e4e2d38b
Runarok <RunarokHrafn@gmail.com>
2025-02-18 17:44:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Print With Separator/print-with-separator.java
+++ Difficulty: Basic/Print With Separator/print-with-separator.java
@@ -1,23 +1,37 @@
 //{ Driver Code Starts
+// Initial Template for Java
 
+import java.util.Scanner;
+
+class Main {
+    static Scanner scn = new Scanner(System.in);
+
+    
 // } Driver Code Ends
 
-import java.util.*;
-
-public class GFG {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-
-        // Read input strings
-        String firstString = sc.nextLine();
-        String secondString = sc.nextLine();
-        String separator = sc.nextLine();
-
-        // Print concatenated output with the separator in between
-        System.out.println(firstString + separator + secondString);
-    }
+// User function Template for Java
+public static void utility() {
+    // Reading input strings a, b, and separator from user
+    String a = scn.nextLine();
+    String b = scn.nextLine();
+    String separator = scn.nextLine();
+    
+    // Concatenating the strings with the separator and printing the result
+    System.out.println(a + separator + b);
 }
 
 
 //{ Driver Code Starts.
+
+    public static void main(String[] args) {
+        int t = scn.nextInt();
+        scn.nextLine();
+        while (t-- > 0) {
+            utility();
+
+            System.out.println("~");
+        }
+        scn.close();
+    }
+}
 // } Driver Code Ends
