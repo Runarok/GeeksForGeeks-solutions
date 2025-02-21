57def19be6aa81209113cd3e99a52a4ad5e58fff
Runarok <RunarokHrafn@gmail.com>
2025-02-21 16:42:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/For-Each Loop/for-each-loop.java
@@ -0,0 +1,25 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+// Back-end complete function Template for Java
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // Creating a scanner object to read input
+        Scanner sc = new Scanner(System.in);
+        
+        // Reading the input string and splitting it into individual words
+        String[] input = sc.nextLine().split(" ");
+        
+        // Iterating through each word in the array and printing it
+        for (String each : input) {
+            System.out.println(each);
+        }
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
