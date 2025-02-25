98c8296d3dd9e8ca10acc1cbbbdca5886f3b3f1b
Runarok <RunarokHrafn@gmail.com>
2025-02-25 18:12:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Swap The Numbers/swap-the-numbers.java
@@ -0,0 +1,25 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+// User function Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        // Create a Scanner object to read input from the user
+        Scanner scanner = new Scanner(System.in);
+
+        // Read two integer values from the user
+        int firstNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+
+        // Swap and print the numbers (without using a temporary variable)
+        System.out.println(secondNumber + " " + firstNumber);
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
