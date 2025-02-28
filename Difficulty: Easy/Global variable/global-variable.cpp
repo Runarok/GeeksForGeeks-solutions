6c9c2560846e4f70acec245b4087184c9f349039
Runarok <RunarokHrafn@gmail.com>
2025-02-28 15:32:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Global variable/global-variable.cpp
@@ -0,0 +1,21 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+#include <iostream>
+using namespace std;
+
+// Declare a global variable g
+int g = 10;
+
+int main() {
+    // Print the value of the global variable g
+    cout << g << endl;
+
+    return 0;
+}
+
+
+//{ Driver Code Starts.
+
+// } Driver Code Ends
