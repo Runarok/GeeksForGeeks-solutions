980797aba96a5689e8aed08a6c820c4967563db9
Runarok <RunarokHrafn@gmail.com>
2025-02-28 14:50:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Digits/count-digits.java
@@ -0,0 +1,19 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+// Back-end complete function Template for Java
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();  // Input integer
+        String s = String.valueOf(n);  // Convert integer to string
+        System.out.println(s.length());  // Output the length of the string representation
+    }
+}
+
+//{ Driver Code Starts.
+// } Driver Code Ends
