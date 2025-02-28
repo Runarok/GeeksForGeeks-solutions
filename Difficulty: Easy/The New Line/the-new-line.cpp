2a47f4e67cee9910c13a92047f4fdc32d630388e
Runarok <RunarokHrafn@gmail.com>
2025-02-28 15:12:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/The New Line/the-new-line.cpp
@@ -0,0 +1,19 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Print "Geeks for Geeks" in separate lines
+    cout << "Geeks" << endl;
+    cout << "for" << endl;
+    cout << "Geeks" << endl;
+    return 0;
+}
+
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
