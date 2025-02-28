be53213e3137c28008bb1f358a61ffa1e481f803
Runarok <RunarokHrafn@gmail.com>
2025-02-28 15:27:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Learn to Comment - Java/learn-to-comment-java.java
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        // Prints the value of 'a'
+        System.out.println(a);
+
+        // The following line is commented out to prevent the output of 'b'
+        // System.out.println(b);
+
+        // Prints the value of 'c'
+        System.out.println(c);
+    }
+}
+
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
