ecf2f1b11822834edfd57f03b0cf9e79c69d92bd
Runarok <RunarokHrafn@gmail.com>
2025-02-28 15:04:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Type Conversion/type-conversion.java
@@ -0,0 +1,24 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        // Create scanner object to read input from the user
+        Scanner sc = new Scanner(System.in);
+        
+        // Read a double value from the user
+        double d = sc.nextDouble();
+        
+        // Typecast the double value to an integer and print it
+        int i = (int) d;
+        System.out.println(i);
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
