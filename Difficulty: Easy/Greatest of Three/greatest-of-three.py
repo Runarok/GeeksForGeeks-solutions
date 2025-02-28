45eb0c6a797e84196451f0ceb8f814e6e309e2ed
Runarok <RunarokHrafn@gmail.com>
2025-02-28 16:29:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Greatest of Three/greatest-of-three.py
@@ -0,0 +1,23 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+# User function Template for python3
+# Take a, b, and c input and print the greatest of three
+
+# Taking inputs for the three numbers
+a = int(input())
+b = int(input())
+c = int(input())
+
+# Output the maximum of the three numbers
+print(max(a, b, c))
+
+
+
+#{ 
+ # Driver Code Starts.
+
+# } Driver Code Ends
