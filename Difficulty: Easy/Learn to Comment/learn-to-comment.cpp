8f609d399362a8bb3716cb3a098653fb1a57bde7
Runarok <RunarokHrafn@gmail.com>
2025-02-28 15:27:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Learn to Comment/learn-to-comment.cpp
@@ -0,0 +1,28 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    // Output the value of 'a'
+    cout << a << " ";
+   
+    // The following line is commented out to prevent the output of 'b'
+    // cout << b << " ";
+    
+    // Output the value of 'c'
+    cout << c << " ";
+    cout << endl;
+
+    return 0;
+}
+
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
