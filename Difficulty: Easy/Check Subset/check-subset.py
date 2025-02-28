13e61471e566c3eab141ca3f00f907b01f0b8e97
Runarok <RunarokHrafn@gmail.com>
2025-02-28 15:13:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check Subset/check-subset.py
@@ -0,0 +1,20 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+a = set(list(map(int, input().split())))
+b = set(list(map(int, input().split())))
+
+# Check if 'a' is a subset of 'b'
+res = a.issubset(b)
+
+# Print True or False
+print(res)
+
+
+
+#{ 
+ # Driver Code Starts.
+# } Driver Code Ends
