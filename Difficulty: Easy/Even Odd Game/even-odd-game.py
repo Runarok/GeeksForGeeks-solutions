0c99dfa0b2f0fd9ae8e978895ce813498f550ff5
Runarok <RunarokHrafn@gmail.com>
2025-02-28 15:16:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Even Odd Game/even-odd-game.py
@@ -0,0 +1,21 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+
+# User function Template for python3
+# Take n input and print who wins
+n = int(input())
+
+if n % 2 == 1:
+    print("You")
+else:
+    print("Friend")
+
+
+#{ 
+ # Driver Code Starts.
+
+# } Driver Code Ends
