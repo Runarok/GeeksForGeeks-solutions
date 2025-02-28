d365d2f84990e43fc6f85f43613212fcea3e6199
Runarok <RunarokHrafn@gmail.com>
2025-02-28 15:24:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Binary to Decimal/binary-to-decimal.cpp
+++ Difficulty: Easy/Binary to Decimal/binary-to-decimal.cpp
@@ -1,44 +1,36 @@
 //{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
 
 // } Driver Code Ends
 
-class Solution {
-  public:
-    // Function to convert binary number (given as integer) to decimal
-    int binary_to_decimal(int n) {
-        int decimalValue = 0;  // To store the final decimal value
-        int base = 1;          // The base starts from 1 (2^0)
+#include <iostream>
+using namespace std;
 
-        // Loop until n becomes 0
-        while (n > 0) {
-            int lastDigit = n % 10;  // Get the last digit (either 0 or 1)
-            decimalValue = decimalValue + (lastDigit * base); // Add the corresponding decimal value
-            n = n / 10;  // Remove the last digit
-            base = base * 2; // Move to the next higher power of 2
-        }
+// Function to convert a binary number (in long long format) to decimal
+int binaryToDecimal(long long n) {
+    int decimal = 0;  // Variable to store the decimal result
+    int base = 1;     // Base multiplier (starting from 2^0)
 
-        return decimalValue;  // Return the final decimal value
+    // Process each digit of the binary number
+    while (n > 0) {
+        int last_digit = n % 10;   // Get the last digit of the binary number
+        decimal += last_digit * base;  // Add the corresponding value to decimal
+        base *= 2;  // Multiply the base by 2 for the next binary digit place
+        n /= 10;     // Remove the last digit of the binary number
     }
-};
+    return decimal;  // Return the final decimal result
+}
+
+int main() {
+    long long binary;  // Variable to hold the input binary number
+    
+    cin >> binary;  // Take the binary number input
+    
+    cout << binaryToDecimal(binary) << endl;  // Output the decimal equivalent of the binary number
+
+    return 0;
+}
+
 
 
 //{ Driver Code Starts.
-int main() {
-    int T;
-    cin >> T;
-    while (T--) {
-        int B;
-        cin >> B;
-        Solution ob;
-        int ans = ob.binary_to_decimal(B);
-        cout << ans << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
 // } Driver Code Ends
