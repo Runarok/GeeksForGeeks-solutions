7de3ce93cf56445dbc83368069495bac2a64a683
Runarok <RunarokHrafn@gmail.com>
2025-03-01 18:40:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Range of Composite numbers/range-of-composite-numbers.py
@@ -0,0 +1,21 @@
+# User function Template for python3
+
+def Range(n):
+    # Returns 1 for now as a placeholder; implementation will be added here
+    return 1
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t=int(input())
+
+    for _ in range(t):
+        n=int(input())
+
+        print(Range(n))
+        print("~")
+# } Driver Code Ends
