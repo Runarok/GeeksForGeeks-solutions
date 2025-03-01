f469a6024f5ccd8e6a97556bc67aa79dddd8375f
Runarok <RunarokHrafn@gmail.com>
2025-03-01 16:46:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/The Size of Operator/the-size-of-operator.cpp
@@ -0,0 +1,26 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    float b;
+    double c;
+    long long d;
+
+    cin >> a >> b >> c >> d;
+    
+    // Print the size of each variable type using sizeof() operator
+    printf("%lu %lu %lu %lu \n", sizeof(a), sizeof(b), sizeof(c), sizeof(d));
+
+    return 0;
+}
+
+
+
+//{ Driver Code Starts.
+
+// } Driver Code Ends
