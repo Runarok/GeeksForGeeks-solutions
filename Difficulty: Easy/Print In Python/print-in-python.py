159fda8786831ce175019f7a2cd20db1f4a7b61f
Runarok <RunarokHrafn@gmail.com>
2025-03-01 18:34:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print In Python/print-in-python.py
@@ -0,0 +1,31 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+# User function Template for python3
+a = input()
+b = input()
+separator = input()
+
+########### Write your code below ###############
+# Print with space
+print(a, b)
+
+# Print without newline at the end
+print(a, b, end='')
+
+# Print with separator
+print(a + separator + b)
+
+# Print without space
+print(a + b)
+########### Write your code above ###############
+
+
+
+#{ 
+ # Driver Code Starts.
+
+# } Driver Code Ends
