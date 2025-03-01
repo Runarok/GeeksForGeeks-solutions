3cdaacfafe61cdaba3d882bd045e72b749ebe714
Runarok <RunarokHrafn@gmail.com>
2025-03-02 03:27:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Right Angle Triangle 2/right-angle-triangle-2.java
+++ Difficulty: Easy/Right Angle Triangle 2/right-angle-triangle-2.java
@@ -1,37 +1,44 @@
 //{ Driver Code Starts
+// Initial Template for Java
 
+import java.util.Scanner;
+
+class Main {
+
+    
 // } Driver Code Ends
 
-import java.util.*;
+// User function Template for Java
 
-public class Main {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        
-        // Loop to iterate through rows
-        for (int i = 1; i <= n; i++) {
-            // Loop to iterate through columns in each row
-            for (int j = 1; j <= i; j++) {
-                // Conditions for printing '*'
-                // 1. First column
-                // 2. Last column of the current row
-                // 3. Last row (bottom of triangle)
-                if (i == n || j == 1 || i == j) {
-                    System.out.print("* ");
-                } 
-                // Otherwise, print space to create the hollow effect
-                else {
-                    System.out.print("  ");
-                }
+public static void triangle(int s) {
+    // Loop through each row starting from the bottom
+    for (int i = s - 1; i >= 0; i--) {
+        // Loop through each column in the current row
+        for (int j = 0; j < s - i; j++) {
+            // Print '*' if it's on the boundary of the triangle (first column, last row, or diagonal)
+            if (j == 0 || i == 0 || i + j == s - 1) {
+                System.out.print("* ");
+            } else {
+                // Otherwise, print spaces to form the triangle shape
+                System.out.print("  ");
             }
-            // Move to the next line after each row
-            System.out.println();
         }
+        // Move to the next line after printing each row
+        System.out.println();
     }
 }
 
 
-
 //{ Driver Code Starts.
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        while (t-- > 0) {
+            int s = scn.nextInt();
+            triangle(s);
+        }
+        scn.close();
+    }
+}
 // } Driver Code Ends
