878eb051d3a3ad3f3c9e71747eec3ba93acbe801
Runarok <RunarokHrafn@gmail.com>
2025-03-01 10:58:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Scientific Format/scientific-format.cpp
@@ -0,0 +1,27 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Declare a double variable to store input
+    double a;
+    
+    // Read the input value
+    cin >> a;
+    
+    // Set the output to scientific notation with 4 decimal places
+    cout << scientific << setprecision(4);
+    
+    // Output the value in the specified format
+    cout << a;
+    
+    return 0;
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
