e810362772999308c8bbbe3b1cdab40094d09143
Runarok <RunarokHrafn@gmail.com>
2025-03-01 12:46:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Add - Auto Keyword/add-auto-keyword.cpp
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare variables
+    int a;
+    float b;
+    long c;
+    
+    // Input values for a, b, and c
+    cin >> a >> b >> c;
+
+    // Compute the sum of a, b, and c
+    auto ans = a + b + c;
+
+    // Print the computed result
+    cout << ans << endl;
+    
+    return 0;
+}
+
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
